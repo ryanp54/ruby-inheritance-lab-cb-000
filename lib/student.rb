f753d54b6ea6c085a6ccb29b8fb87beacfff9789
Ryan Pfeifer <pfeifer.ryan@gmail.com>
2017-11-10 01:30:25 +0000

    automatically backed up by learn

--- lib/student.rb
+++ lib/student.rb
@@ -1,3 +1,16 @@
-class Student 
+require 'lib/user.rb'
 
+class Student < User
+  attr_accessor :knowledge
+  
+  def initialize(first, last)
+    @first_name = first
+    @last_name = last
+    @knowledge = []
+  end
+
+  def learn(string)
+    knowledge << string    
+  end
+  
 end
